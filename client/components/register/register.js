97e545806b4ebedc3f8bfe511f2f4e1257b32cb0
nangelos <nicksangelos@gmail.com>
2020-05-08 14:49:28 -0400

    [register] button & styling for adding another student

--- client/components/register/register.js
+++ client/components/register/register.js
@@ -13,20 +13,35 @@ import {
 } from '../../constants'
 import StudentInfo from './student-info'
 
-class Register extends Component {
-  state = {}
+const AddButton = styled.input`
+  background: black;
+  color: white;
+  font-size: x-large;
+  border-radius: 5px;
+  height: 40px;
+  width: 40px;
+  &:hover {
+    opacity: 0.5;
+    border: white solid 2px;
+  }
+`
 
-  handleTextboxChange = (evt) => {
-    let {name, value} = evt.target
-    this.setState({[name]: value})
-    console.log(this.state)
+class Register extends Component {
+  state = {
+    addButtonHovered: false,
   }
 
-  changeBinary = (evt, name, value) => {
+  onMouseEnter = (e) => {
+    this.setState({addButtonHovered: true})
+  }
+
+  onMouseLeave = (e) => {
+    this.setState({addButtonHovered: false})
+  }
+
+  addStudent = (evt) => {
     evt.preventDefault()
-    let newName = `${name.toLowerCase()}Registered`
-    let newValue = !JSON.parse(value)
-    this.setState({[newName]: newValue})
+    console.log('Add another student component')
   }
 
   handleSubmit = (evt) => {
@@ -35,11 +50,32 @@ class Register extends Component {
   }
 
   render() {
+    const {addButtonHovered} = this.state
+    const style = addButtonHovered
+      ? {}
+      : {visibility: 'hidden', paddingLeft: '20px'}
     return (
       <div style={{textAlign: 'center'}}>
         <h2 style={{textAlign: 'left'}}>Register Your Student</h2>
         <ParentInfo />
         <StudentInfo />
+        <div style={{textAlign: 'center'}}>
+          <div style={{display: 'inline-block', width: '90%'}}>
+            <div
+              style={{textAlign: 'left', display: 'flex', alignItems: 'center'}}
+            >
+              <AddButton
+                type="submit"
+                value="+"
+                onClick={this.addStudent}
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+              ></AddButton>
+              <p style={style}>Click to Add Student</p>
+            </div>
+          </div>
+        </div>
+        <p />
         <input
           type="submit"
           value="Register"
