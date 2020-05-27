9b7e7c76a19bc09d81390027e75a842eaa3106cd
nangelos <nicksangelos@gmail.com>
2020-05-27 18:04:14 -0400

    [settings] beginnings of users ability to update info

---
+++ client/components/settings/settings.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import {changeParentInfo, changeStudentInfo} from '../../store'
+import {secondaryColor, primaryColor} from '../../constants'
+import ParentInfo from '../register/parent-info'
+import StudentInfo from '../register/student-info'
+
+const SettingsWrapper = styled.div`
+  background: ${secondaryColor};
+  margin: 10px;
+  padding: 5px;
+  // display: inline-block;
+  width: 70%;
+`
+const Sidebar = styled.div`
+  width: 20%;
+  text-align: left;
+  display: flex;
+  background: ${primaryColor};
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+
+const Tab = styled.button`
+  font-weight: ${(state) => (state.active ? 'bold' : 'normal')};
+  background: ${(state) =>
+    state.active ? `rgba(50, 52, 52, 0.8)` : `rgba(50, 52, 52, 0.3)`};
+  padding: 1em 0.5em;
+  width: 100%;
+  border: none;
+  color: white;
+  font-size: large;
+  &:hover {
+    opacity: 0.9;
+    cursor: pointer;
+  }
+`
+
+class Settings extends Component {
+  state = {
+    active: 'ParentInfo',
+    students: [],
+  }
+  userId = 1
+
+  changeActive = (evt) => {
+    evt.preventDefault()
+    console.log(evt.target.value)
+    const {value} = evt.target
+    this.setState({active: value})
+  }
+
+  render() {
+    console.log(ParentInfo)
+    const {active} = this.state
+    return (
+      <div style={{textAlign: 'center'}}>
+        <div style={{display: 'flex'}}>
+          <Sidebar>
+            <Tab
+              onClick={this.changeActive}
+              active={this.state.active === 'ParentInfo'}
+              value="ParentInfo"
+            >
+              Parent Info
+            </Tab>
+            <Tab
+              onClick={this.changeActive}
+              active={this.state.active === 'StudentInfo'}
+              value="StudentInfo"
+            >
+              Student Info
+            </Tab>
+            <Tab
+              onClick={this.changeActive}
+              active={this.state.active === 'Payment'}
+              value="Payment"
+            >
+              Payment
+            </Tab>
+          </Sidebar>
+          <SettingsWrapper>
+            {active === 'ParentInfo' ? (
+              <ParentInfo />
+            ) : active === 'StudentInfo' ? (
+              <StudentInfo />
+            ) : (
+              <h1>Payment Info</h1>
+            )}
+          </SettingsWrapper>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Settings
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Welcome, Login, Signup, UserHome, Register} from './components'
+import {
+  Welcome,
+  Login,
+  Signup,
+  UserHome,
+  Register,
+  Settings,
+} from './components'
 import {me} from './store'
 
 /**
@@ -27,6 +34,7 @@ class Routes extends Component {
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
             <Route path="/register" component={Register} />
+            <Route path="/settings" component={Settings} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
@@ -39,19 +47,19 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
   }
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
       dispatch(me())
-    }
+    },
   }
 }
 
@@ -64,5 +72,5 @@ export default withRouter(connect(mapState, mapDispatch)(Routes))
  */
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
 }
