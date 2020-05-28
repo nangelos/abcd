f70186585deda333aa14e36e28168e90b7cf1052
nangelos <nicksangelos@gmail.com>
2020-05-28 17:57:15 -0400

    [parent-update] fetchParentInfo & render + code cleanup

--- client/components/settings/parent-update.js
+++ client/components/settings/parent-update.js
@@ -40,8 +40,7 @@ class ParentUpdate extends Component {
     this.setState({[name]: value})
   }
 
-  handleSubmit = (evt) => {
-    evt.preventDefault()
+  handleSubmit = () => {
     const {updateParentInfo, userId} = this.props
     const list = this.state
     const filtered = Object.keys(list)
@@ -52,135 +51,154 @@ class ParentUpdate extends Component {
           [key]: list[key],
         }
       }, {})
-    console.log('filtered state: ', filtered)
     updateParentInfo(userId, filtered)
   }
 
   componentDidMount() {
-    // NEED TO FETCH PARENT INFO, INLCUDING PASSING DOWN userId
+    const {userId, getParentInfo} = this.props
+    getParentInfo(userId)
   }
 
   render() {
-    const props = this.props
-    console.log(props)
+    const parent = this.props.state.parent[0]
     return (
       <div style={{textAlign: 'center'}}>
-        <h2>Parent Information</h2>
-        <ParentWrapper>
-          <form onSubmit={this.handleSubmit}>
-            <InfoRow>
-              <p style={{width: '120px'}}>Full Name</p>
-              <InfoInput
-                name="parentFirst"
-                placeholder="Parent First Name"
-                onChange={this.handleTextboxChange}
-              />
-              <InfoInput
-                name="parentLast"
-                placeholder="Parent Last Name"
-                onChange={this.handleTextboxChange}
-              />
-            </InfoRow>
-            <InfoRow>
-              <p style={{width: '120px'}}>Contact Info</p>
-              <InfoInput
-                name="parentCell"
-                placeholder="Cell Phone"
-                style={{width: '180px'}}
-                onChange={this.handleTextboxChange}
-              />
-              <InfoInput
-                name="parentWork"
-                placeholder="Work Phone"
-                style={{width: '180px'}}
-                onChange={this.handleTextboxChange}
-              />
-              <InfoInput
-                name="parentEmail"
-                placeholder="Email Address"
-                onChange={this.handleTextboxChange}
-              />
-            </InfoRow>
-            <InfoRow>
-              <p style={{width: '120px'}}>Address</p>
-              <InfoInput
-                name="parentAddress"
-                placeholder="Street Address"
-                style={{width: '500px'}}
-                onChange={this.handleTextboxChange}
-              />
-            </InfoRow>
-            <InfoRow style={{alignItems: 'center'}}>
-              <p style={{width: '120px'}}></p>
-              <InfoInput
-                name="parentCity"
-                placeholder="City"
-                onChange={this.handleTextboxChange}
-              />
-              <select
-                name="parentState"
-                onChange={this.handleTextboxChange}
-                style={{fontSize: 'large', height: '30px'}}
-              >
-                {stateList.map((st, i) => {
-                  return (
-                    <option key={i} value={st}>
-                      {st}
-                    </option>
-                  )
-                })}
-              </select>
-              <InfoInput
-                name="parentZip"
-                placeholder="Zip Code"
-                style={{width: '90px'}}
-                onChange={this.handleTextboxChange}
-              />
-            </InfoRow>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-              <div style={{margin: '0px'}}>
-                <p style={{marginBottom: '8px', width: '120px'}}>Emergency</p>
-                <p style={{marginTop: '8px'}}>Contacts</p>
+        {parent ? (
+          <ParentWrapper>
+            <form onSubmit={this.handleSubmit}>
+              <InfoRow>
+                <p style={{width: '120px'}}>Full Name</p>
+                <InfoInput
+                  name="parentFirst"
+                  placeholder={parent.parentFirst}
+                  onChange={this.handleTextboxChange}
+                />
+                <InfoInput
+                  name="parentLast"
+                  placeholder={parent.parentLast}
+                  onChange={this.handleTextboxChange}
+                />
+              </InfoRow>
+              <InfoRow>
+                <p style={{width: '120px'}}>Contact Info</p>
+                <InfoInput
+                  name="parentCell"
+                  placeholder={parent.parentCell}
+                  style={{width: '180px'}}
+                  onChange={this.handleTextboxChange}
+                />
+                <InfoInput
+                  name="parentWork"
+                  placeholder={
+                    parent.parentWork ? parent.parentWork : 'Work Phone'
+                  }
+                  style={{width: '180px'}}
+                  onChange={this.handleTextboxChange}
+                />
+                <InfoInput
+                  name="parentEmail"
+                  placeholder={
+                    parent.parentEmail ? parent.parentEmail : 'Email Address'
+                  }
+                  onChange={this.handleTextboxChange}
+                />
+              </InfoRow>
+              <InfoRow>
+                <p style={{width: '120px'}}>Address</p>
+                <InfoInput
+                  name="parentAddress"
+                  placeholder={
+                    parent.parentAddress
+                      ? parent.parentAddress
+                      : 'Street Address'
+                  }
+                  style={{width: '500px'}}
+                  onChange={this.handleTextboxChange}
+                />
+              </InfoRow>
+              <InfoRow style={{alignItems: 'center'}}>
+                <p style={{width: '120px'}}></p>
+                <InfoInput
+                  name="parentCity"
+                  placeholder={parent.parentCity ? parent.parentCity : 'City'}
+                  onChange={this.handleTextboxChange}
+                />
+                <select
+                  name="parentState"
+                  onChange={this.handleTextboxChange}
+                  value={parent.parentState}
+                  style={{fontSize: 'large', height: '30px'}}
+                >
+                  {stateList.map((st, i) => {
+                    return (
+                      <option key={i} value={st}>
+                        {st}
+                      </option>
+                    )
+                  })}
+                </select>
+                <InfoInput
+                  name="parentZip"
+                  placeholder={parent.parentZip}
+                  style={{width: '90px'}}
+                  onChange={this.handleTextboxChange}
+                />
+              </InfoRow>
+              <div style={{display: 'flex', alignItems: 'center'}}>
+                <div style={{margin: '0px'}}>
+                  <p style={{marginBottom: '8px', width: '120px'}}>Emergency</p>
+                  <p style={{marginTop: '8px'}}>Contacts</p>
+                </div>
+                <div style={{margin: '0px'}}>
+                  <InfoRow style={{margin: '0px'}}>
+                    <InfoInput
+                      name="eContactName1"
+                      placeholder={parent.eContactName1}
+                      onChange={this.handleTextboxChange}
+                      style={{width: '400px', marginRight: '50px'}}
+                    />
+                    <InfoInput
+                      name="eContactPhone1"
+                      placeholder={parent.eContactPhone1}
+                      style={{width: '180px'}}
+                      onChange={this.handleTextboxChange}
+                    />
+                  </InfoRow>
+                  <InfoRow style={{margin: '0px'}}>
+                    <InfoInput
+                      name="eContactName2"
+                      placeholder={
+                        parent.eConcactName2
+                          ? parent.eConcactName2
+                          : 'Secondary Contact'
+                      }
+                      style={{width: '400px', marginRight: '50px'}}
+                      onChange={this.handleTextboxChange}
+                    />
+                    <InfoInput
+                      name="eContactPhone2"
+                      placeholder={
+                        parent.eConcactPhone2
+                          ? parent.eConcactPhone2
+                          : 'Phone Number'
+                      }
+                      style={{width: '180px'}}
+                      onChange={this.handleTextboxChange}
+                    />
+                  </InfoRow>
+                </div>
               </div>
-              <div style={{margin: '0px'}}>
-                <InfoRow style={{margin: '0px'}}>
-                  <InfoInput
-                    name="eContactName1"
-                    placeholder="Primary Contact"
-                    onChange={this.handleTextboxChange}
-                    style={{width: '400px', marginRight: '50px'}}
-                  />
-                  <InfoInput
-                    name="eContactPhone1"
-                    placeholder="Phone Number"
-                    style={{width: '180px'}}
-                    onChange={this.handleTextboxChange}
-                  />
-                </InfoRow>
-                <InfoRow style={{margin: '0px'}}>
-                  <InfoInput
-                    name="eContactName2"
-                    placeholder="Secondary Contact"
-                    style={{width: '400px', marginRight: '50px'}}
-                    onChange={this.handleTextboxChange}
-                  />
-                  <InfoInput
-                    name="eContactPhone2"
-                    placeholder="Phone Number"
-                    style={{width: '180px'}}
-                    onChange={this.handleTextboxChange}
-                  />
-                </InfoRow>
-              </div>
-            </div>
-            <input
-              type="submit"
-              value="Update"
-              id="submitButton"
-              style={{background: primaryColor}}
-            />
-          </form>
-        </ParentWrapper>
+              <input
+                type="submit"
+                value="Update"
+                id="submitButton"
+                style={{background: primaryColor}}
+              />
+            </form>
+          </ParentWrapper>
+        ) : (
+          <h4>Loading...</h4>
+        )}
       </div>
     )
   }
