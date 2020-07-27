65b8824fb2e9cd53840905532cabad1d967e4bd2
nangelos <nicksangelos@gmail.com>
2020-07-27 16:45:40 -0400

    [absence] adding absence to store

---
+++ client/store/absence.js
@@ -0,0 +1,93 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_ALL_ABSENCES = 'GET_ALL_ABSENCES'
+const GET_STUDENT_ABSENCES = 'GET_STUDENT_ABSENCES'
+const CREATE_ABSENCE = 'CREATE_ABSENCE'
+const UPDATE_ABSENCE = 'UPDATE_ABSENCE'
+const DELETE_ABSENCE = 'DELETE_ABSENCE'
+
+/**
+ * INITIAL STATE
+ */
+const defaultAbsence = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getAllAbsences = (data) => ({type: GET_ALL_ABSENCES, data})
+const getStudentAbsences = (data) => ({type: GET_STUDENT_ABSENCES, data})
+const createAbsence = (data) => ({type: CREATE_ABSENCE, data})
+const updateAbsence = (data) => ({type: UPDATE_ABSENCE, data})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchAllAbsences = () => async (dispatch) => {
+  try {
+    const res = await axios.get(`api/absences`)
+    dispatch(getAllAbsences(res.data || defaultAbsence))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const fetchStudentAbsences = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`api/absences/${id}`)
+    dispatch(getStudentAbsences(res.data || defaultAbsence))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const addAbsence = (info) => async (dispatch) => {
+  let res
+  console.log('addAbsence: ', info)
+  try {
+    res = await axios.post(`api/absences`, info)
+  } catch (err) {
+    return dispatch(createAbsence({error: err}))
+  }
+  try {
+    dispatch(getStudentAbsences(res.data))
+    // history.push('/schedule')
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
+export const changeAbsence = (id, info) => async (dispatch) => {
+  let res
+  try {
+    res = await axios.put(`api/absences/${id}`, info)
+  } catch (err) {
+    console.error(err)
+  }
+  try {
+    dispatch(updateAbsence(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function (state = defaultAbsence, action) {
+  switch (action.type) {
+    case GET_ALL_ABSENCES:
+      return action.data
+    case GET_STUDENT_ABSENCES:
+      return action.data
+    case CREATE_ABSENCE:
+      return [...state, action.data]
+    case UPDATE_ABSENCE:
+      return action.data
+    default:
+      return state
+  }
+}
--- client/store/index.js
+++ client/store/index.js
@@ -5,8 +5,9 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import parent from './parent'
 import student from './student'
+import absense from './absence'
 
-const reducer = combineReducers({user, parent, student})
+const reducer = combineReducers({user, parent, student, absense})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -16,3 +17,4 @@ export default store
 export * from './user'
 export * from './parent'
 export * from './student'
+export * from './absence'
